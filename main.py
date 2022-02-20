5a1dc260e10abcc26aae6a222f14db8b1ca0afe5
GavaniGorgia <91087295+GavaniGorgia@users.noreply.github.com>
2022-02-20 21:04:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('2119464296:AAHHvLPvNioNKU58ucHa6cA1zeaHhsRUi18')
 
 @bot.message_handler(commands=['hello'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет ,гость,для того,чтобы узнать больше о боте пропишите /help')
+    bot.send_message(message.chat.id, 'Привет ,гость,для того,чтобы узнать больше о боте пропишите /help \r\nТак же если посмотреть что-то про место нашего обучения можешь прописать  /study')
 
 
 @bot.message_handler(commands=['help'])
@@ -21,17 +21,27 @@ def help_mes(message):
     markup.add(btnGif)      
     bot.send_message(message.chat.id, 'Выбери что ты хочешь узнать', reply_markup=markup)
 
-
+@bot.message_handler(commands=['study'])
+def study_mes(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btnSait = types.KeyboardButton('Сайт нашего универа')
+    btnWho = types.KeyboardButton('Сайт кафедры')
+    markup.add(btnSait)
+    markup.add(btnWho)
+    bot.send_message(message.chat.id, 'Выбери что ты хочешь узнать', reply_markup=markup)
 
 @bot.message_handler(content_types='text')
 def reply_message(message):
     if message.text == 'Что мы делаем':
-        bot.send_message(message.chat.id, 'Учимся не покладая рук ,занимаемся саморазвитием и отдыхаем,')
+        bot.send_message(message.chat.id, 'Учимся не покладая рук ,занимаемся саморазвитием и отдыхаем')
     if message.text == 'Секретная кнопка':
         bot.send_message(message.chat.id, constants.random_message())
     if message.text == 'Прикольная гифка':
         bot.send_video(message.chat.id, constants.random_message2())
-
+    if message.text == 'Сайт нашего универа':
+        bot.send_message(message.chat.id, 'https://mtuci.ru/')
+    if message.text == 'Сайт кафедры':
+        bot.send_message(message.chat.id, 'https://mtuci.tech/')
 
 
 
